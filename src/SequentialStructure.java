9c0198f6c6a93c4497ca57a46ee79b117809f82e
Isaque Menezes <isaquesantos1517@gmail.com>
2024-07-29 09:49:17 -0300

    Unindo exercicios iniciais e posteriores.

--- src/SequentialStructure.java
+++ src/SequentialStructure.java
@@ -4,6 +4,160 @@ public class SequentialStructure {
 
     static Scanner input = new Scanner(System.in);
 
+    public static void exerciceOne() {
+        /*
+            Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+         */
+
+        System.out.println("Hello Word!");
+
+    }
+
+    public static void exerciceTwo() {
+        /*
+            Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+         */
+
+        int valor;
+
+        System.out.println("Digite um numero: ");
+        valor = Integer.parseInt(input.nextLine());
+        System.out.println("O numero informado foi: " + valor);
+
+    }
+
+    public static void exerciceThree() {
+        /*
+            Faça um Programa que peça dois números e imprima a soma.
+         */
+
+        int num1;
+        int num2;
+
+        System.out.println("Digite o primeiro numero: ");
+        num1 = Integer.parseInt(input.nextLine());
+
+        System.out.println("Digite o segundo numero: ");
+        num2 = Integer.parseInt(input.nextLine());
+
+        System.out.println("A soma dos dois numeros resulta em: " + (num1 + num2));
+
+    }
+
+    public static void exerciceFour() {
+        /*
+            Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+         */
+
+        double num1;
+        double num2;
+        double num3;
+        double num4;
+
+        System.out.println("Digite a nota do primeiro bimestre.");
+        num1 = Double.parseDouble(input.nextLine());
+
+        System.out.println("Digite a nota do segundo bimestre.");
+        num2 = Double.parseDouble(input.nextLine());
+
+        System.out.println("Digite a nota do terceiro bimestre.");
+        num3 = Double.parseDouble(input.nextLine());
+
+        System.out.println("Digite a nota do quarto bimestre.");
+        num4 = Double.parseDouble(input.nextLine());
+
+        System.out.println("A sua media é: " + ((num1 + num2 + num3 + num4)/4));
+
+    }
+
+    public static void exerciceFive() {
+        /*
+            Faça um Programa que converta metros para centímetros.
+         */
+
+        int metros;
+
+        System.out.println("Digite um valor em metros: ");
+        metros = Integer.parseInt(input.nextLine());
+
+        System.out.printf("%s metros equivalem a %s centimetros.", metros, metros*100);
+
+    }
+
+    public static void exerciceSix() {
+        /*
+            Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+         */
+        int raio;
+
+        System.out.println("Digite o raio do circulo: ");
+        raio = Integer.parseInt(input.nextLine());
+
+        System.out.printf("A area de um circulo com raio %s é: %s.", raio, Math.PI * Math.pow(raio, 2));
+
+    }
+
+    public static void exerciceSeven() {
+        /*
+            Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+         */
+
+        int lado;
+
+        System.out.println("Digite o lado do quadrado: ");
+        lado = Integer.parseInt(input.nextLine());
+
+        System.out.printf("A area de um quadrado de lado %s cm é: %s.", lado, Math.pow(lado, 2));
+
+    }
+
+    public static void exerciceEight() {
+        /*
+            Faça um Programa que pergunte quanto você ganha por hora e o número de
+            horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+         */
+
+        double reaisHora;
+        double horasTrabalhadas;
+
+        System.out.println("Digite quanto você ganha por hora: ");
+        reaisHora = Double.parseDouble(input.nextLine());
+        System.out.println("Digite quantas horas você trabalhou: ");
+        horasTrabalhadas = Double.parseDouble(input.nextLine());
+
+        System.out.println("Você receberá um total de: " + reaisHora * horasTrabalhadas);
+
+    }
+
+    public static void exerciceNine() {
+        /*
+            Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a
+            temperatura em graus Celsius.
+            C = 5 * ((F-32) / 9).
+         */
+
+        double temperatura;
+
+        System.out.println("Digite a temperatura em Fahrenheit: ");
+        temperatura = Double.parseDouble(input.nextLine());
+
+        System.out.printf("%s graus Fahrenheit é equivalente a %s graus Celsius.", temperatura, ((temperatura-32.0) * (5.0 / 9.0)));
+
+    }
+
+    public static void exerciceTen() {
+        /*
+            Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+         */
+
+        double temperatura;
+
+        System.out.println("Digite a temperatura em Celsius: ");
+        temperatura = Double.parseDouble(input.nextLine());
+
+        System.out.printf("%s graus Celsius é equivalente a %s graus Fahrenheit.", temperatura, (temperatura * (9.0 / 5.0)) + 32);
+    }
+
     public static void exerciceEleven() {
         /*
             Faça um Programa que peça 2 números inteiros e um número real.
@@ -115,6 +269,16 @@ public class SequentialStructure {
     }
 
     public static void main(String[] args) {
+        // exerciceOne();
+        // exerciceTwo();
+        // exerciceThree();
+        // exerciceFour();
+        // exerciceFive();
+        // exerciceSix();
+        // exerciceSeven();
+        // exerciceEight();
+        // exerciceNine();
+        // exerciceTen();
         // exerciceEleven();
         // exerciceTwelve();
         // exerciceThirteen();
