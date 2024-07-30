542ca7619b3e9eccedf664dd06228502ffd8aea9
Isaque Menezes <isaquesantos1517@gmail.com>
2024-07-30 16:51:25 -0300

    Exercicio 17 concluido!

--- src/SequentialStructure.java
+++ src/SequentialStructure.java
@@ -320,6 +320,49 @@ public class SequentialStructure {
 
     }
 
+    public static void exerciceSeventeen() {
+        /*
+            Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho
+            em metros quadrados da área a ser pintada. Considere que a cobertura da tinta
+            é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de
+            18 litros, que custam R$ 80,00 ou em galões de 3,6 litros, que custam R$ 25,00.
+            Informe ao usuário as quantidades de tinta a serem compradas e os respectivos
+            preços em 3 situações:
+                comprar apenas latas de 18 litros;
+                comprar apenas galões de 3,6 litros;
+                misturar latas e galões, de forma que o desperdício de tinta seja menor.
+                Acrescente 10% de folga e sempre arredonde os valores para cima, isto é,
+                considere latas cheias.
+         */
+
+        double area;
+        double litrosTinta;
+        double qtdLatasMaior;
+        double qtdLatasMenor;
+
+        System.out.println("Digite a area a ser pintada: ");
+        area = Double.parseDouble(input.nextLine());
+
+        litrosTinta = Math.ceil((area / 6) * 1.10);
+
+        System.out.println("Para essa area, é necessario " + litrosTinta + " litros de tinta.");
+
+        qtdLatasMaior = Math.ceil(litrosTinta / 18);
+        qtdLatasMenor = Math.ceil(litrosTinta / 3.6);
+
+        System.out.println("Se quiser comprar apenas galoes de 18 litros, sera necessario " + qtdLatasMaior + " latas, custando R$ " + qtdLatasMaior * 80.0);
+        System.out.println("Se quiser comprar apenas galoes de 3.6 litros, sera necessario " + qtdLatasMenor + " latas, custando R$ " + qtdLatasMenor * 25.0);
+
+        qtdLatasMaior = Math.floor(litrosTinta / 18);
+        qtdLatasMenor = Math.ceil((litrosTinta - (qtdLatasMaior * 18)) / 3.6);
+
+        System.out.println("Se quiser comprar galoes mistos, sera necessario " +
+                qtdLatasMaior + " latas de 18 litros e " +
+                qtdLatasMenor + " latas de 3.6 litros, custando R$ " +
+                ((qtdLatasMaior * 80.0) + (qtdLatasMenor * 25.0)));
+
+    }
+
     public static void main(String[] args) {
         // exerciceOne();
         // exerciceTwo();
@@ -336,7 +379,8 @@ public class SequentialStructure {
         // exerciceThirteen();
         // exerciceFourteen();
         // exerciceFifteen();
-        exerciceSixteen();
+        // exerciceSixteen();
+        exerciceSeventeen();
     }
 
 }
