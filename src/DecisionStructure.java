67ec09ec7562ba8ed858445d24d2c2aad2c3b067
Isaque Menezes <isaquesantos1517@gmail.com>
2024-08-05 12:13:13 -0300

    Criação de estrutura com exercicios pendente

--- src/DecisionStructure.java
+++ src/DecisionStructure.java
@@ -458,6 +458,221 @@ public class DecisionStructure {
 
     }
 
+    public static void exerciceFourteen() {
+        /*
+            Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um
+            semestre, e calcule a sua média. A atribuição de conceitos obedece à tabela abaixo:
+              Média de Aproveitamento  Conceito
+              Entre 9.0 e 10.0        A
+              Entre 7.5 e 9.0         B
+              Entre 6.0 e 7.5         C
+              Entre 4.0 e 6.0         D
+              Entre 4.0 e zero        E
+            O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” se
+            o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E.
+         */
+
+
+
+    }
+
+    public static void exerciceFifteen() {
+        /*
+            Faça um Programa que peça os 3 lados de um triângulo. O programa deverá informar se os valores podem
+            ser um triângulo. Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles
+            ou escaleno.
+
+            Dicas:
+                Três lados formam um triângulo quando a soma de quaisquer dois lados for maior que o terceiro;
+                Triângulo Equilátero: três lados iguais;
+                Triângulo Isósceles: quaisquer dois lados iguais;
+                Triângulo Escaleno: três lados diferentes;
+         */
+
+
+
+    }
+
+    public static void exerciceSixteen() {
+        /*
+            Faça um programa que calcule as raízes de uma equação do segundo grau, na forma ax2 + bx + c.
+            O programa deverá pedir os valores de a, b e c e fazer as consistências, informando ao usuário nas
+            seguintes situações:
+                Se o usuário informar o valor de A igual a zero, a equação não é do segundo grau e o programa não
+                deve fazer pedir os demais valores, sendo encerrado;
+                Se o delta calculado for negativo, a equação não possui raizes reais. Informe ao usuário e
+                encerre o programa;
+                Se o delta calculado for igual a zero a equação possui apenas uma raiz real; informe-a ao usuário;
+                Se o delta for positivo, a equação possui duas raiz reais; informe-as ao usuário;
+         */
+
+
+
+    }
+
+    public static void exerciceSeventeen() {
+        /*
+            Faça um Programa que peça um número correspondente a um determinado ano e em seguida
+            informe se este ano é ou não bissexto.
+         */
+
+
+
+    }
+
+    public static void exerciceEighteen() {
+        /*
+            Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida.
+         */
+
+
+
+    }
+
+    public static void exerciceNineteen() {
+        /*
+            Faça um Programa que leia um número inteiro menor que 1000 e imprima a quantidade de centenas,
+            dezenas e unidades do mesmo.
+            Observando os termos no plural a colocação do "e", da vírgula entre outros. Exemplo:
+                326 = 3 centenas, 2 dezenas e 6 unidades
+                12 = 1 dezena e 2 unidades Testar com: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10,
+                21, 11, 1, 7 e 16
+         */
+
+
+
+    }
+
+    public static void exerciceTwenty() {
+        /*
+            Faça um Programa para leitura de três notas parciais de um aluno. O programa deve calcular a média
+            alcançada por aluno e presentar:
+                A mensagem "Aprovado", se a média for maior ou igual a 7, com a respectiva média alcançada;
+                A mensagem "Reprovado", se a média for menor do que 7, com a respectiva média alcançada;
+                A mensagem "Aprovado com Distinção", se a média for igual a 10.
+         */
+
+
+
+    }
+
+    public static void exerciceTwentyOne() {
+        /*
+            Faça um Programa para um caixa eletrônico. O programa deverá perguntar ao usuário a valor do saque e
+            depois informar quantas notas de cada valor serão fornecidas. As notas disponíveis serão as de 1, 5, 10,
+            50 e 100 reais. O valor mínimo é de 10 reais e o máximo de 600 reais. O programa não deve se preocupar com
+            a quantidade de notas existentes na máquina.
+                Exemplo 1: Para sacar a quantia de 256 reais, o programa fornece duas notas de 100, uma nota de 50,
+                uma nota de 5 e uma nota de 1;
+                Exemplo 2: Para sacar a quantia de 399 reais, o programa fornece três notas de 100, uma nota de 50,
+                quatro notas de 10, uma nota de 5 e quatro notas de 1.
+         */
+
+
+
+    }
+
+    public static void exerciceTwentyTwo() {
+        /*
+            Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o
+            operador módulo (resto da divisão).
+         */
+
+
+
+    }
+
+    public static void exerciceTwentyThree() {
+        /*
+            Faça um Programa que peça um número e informe se o número é inteiro ou decimal.
+            Dica: utilize uma função de arredondamento.
+         */
+
+
+
+    }
+
+    public static void exerciceTwentyFour() {
+        /*
+            Faça um Programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar.
+            O resultado da operação deve ser acompanhado de uma frase que diga se o número é:
+                par ou ímpar;
+                positivo ou negativo;
+                inteiro ou decimal.
+         */
+
+
+
+    }
+
+    public static void exerciceTwentyFive() {
+        /*
+            Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
+                "Telefonou para a vítima?"
+                "Esteve no local do crime?"
+                "Mora perto da vítima?"
+                "Devia para a vítima?"
+                "Já trabalhou com a vítima?" O programa deve no final emitir uma classificação sobre a participação da
+                pessoa no crime. Se a pessoa responder positivamente a 2 questões ela deve ser classificada como
+                "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado
+                como "Inocente".
+         */
+
+
+
+    }
+
+    public static void exerciceTwentySix() {
+        /*
+            Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+                Álcool:
+                    até 20 litros, desconto de 3% por litro
+                    acima de 20 litros, desconto de 5% por litro
+                Gasolina:
+                    até 20 litros, desconto de 4% por litro
+                    acima de 20 litros, desconto de 6% por litro
+            Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível (codificado da seguinte
+            forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço
+            do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90.
+         */
+
+
+
+    }
+
+    public static void exerciceTwentySeven() {
+        /*
+            Uma fruteira está vendendo frutas com a seguinte tabela de preços:
+                                      Até 5 Kg           Acima de 5 Kg
+                Morango         R$ 2,50 por Kg          R$ 2,20 por Kg
+                Maçã            R$ 1,80 por Kg          R$ 1,50 por Kg
+            Se o cliente comprar mais de 8 Kg em frutas ou o valor total da compra ultrapassar R$ 25,00, receberá
+            ainda um desconto de 10% sobre este total. Escreva um algoritmo para ler a quantidade (em Kg) de
+            morangos e a quantidade (em Kg) de maças adquiridas e escreva o valor a ser pago pelo cliente.
+         */
+
+
+
+    }
+
+    public static void exerciceTwentyEight() {
+        /*
+            O Hipermercado Tabajara está com uma promoção de carnes que é imperdível. Confira:
+                                      Até 5 Kg           Acima de 5 Kg
+                File Duplo      R$ 4,90 por Kg          R$ 5,80 por Kg
+                Alcatra         R$ 5,90 por Kg          R$ 6,80 por Kg
+                Picanha         R$ 6,90 por Kg          R$ 7,80 por Kg
+            Para atender a todos os clientes, cada cliente poderá levar apenas um dos tipos de carne da promoção,
+            porém não há limites para a quantidade de carne por cliente. Se compra for feita no cartão Tabajara
+            o cliente receberá ainda um desconto de 5% sobre o total da compra. Escreva um programa que peça o
+            tipo e a quantidade de carne comprada pelo usuário e gere um cupom fiscal, contendo as informações da
+            compra: tipo e quantidade de carne, preço total, tipo de pagamento, valor do desconto e valor a pagar.
+         */
+
+
+
+    }
+
     public static void main(String[] args) {
         System.out.println("-------- Estruturas de Decisao -------\n\n");
 //        System.out.println("---------- Exercicio 01 --------------\n");
@@ -486,7 +701,36 @@ public class DecisionStructure {
 //        exerciceTwelve();
         System.out.println("---------- Exercicio 13 --------------\n");
         exerciceThirteen();
-
+//        System.out.println("---------- Exercicio 14 --------------\n");
+//        exerciceFourteen();
+//        System.out.println("---------- Exercicio 15 --------------\n");
+//        exerciceFifteen();
+//        System.out.println("---------- Exercicio 16 --------------\n");
+//        exerciceSixteen();
+//        System.out.println("---------- Exercicio 17 --------------\n");
+//        exerciceSeventeen();
+//        System.out.println("---------- Exercicio 18 --------------\n");
+//        exerciceEighteen();
+//        System.out.println("---------- Exercicio 19 --------------\n");
+//        exerciceNineteen();
+//        System.out.println("---------- Exercicio 20 --------------\n");
+//        exerciceTwenty();
+//        System.out.println("---------- Exercicio 21 --------------\n");
+//        exerciceTwentyOne();
+//        System.out.println("---------- Exercicio 22 --------------\n");
+//        exerciceTwentyTwo();
+//        System.out.println("---------- Exercicio 23 --------------\n");
+//        exerciceTwentyThree();
+//        System.out.println("---------- Exercicio 24 --------------\n");
+//        exerciceTwentyFour();
+//        System.out.println("---------- Exercicio 25 --------------\n");
+//        exerciceTwentyFive();
+//        System.out.println("---------- Exercicio 26 --------------\n");
+//        exerciceTwentySix();
+//        System.out.println("---------- Exercicio 27 --------------\n");
+//        exerciceTwentySeven();
+//        System.out.println("---------- Exercicio 28 --------------\n");
+//        exerciceTwentyEight();
     }
 
 }
