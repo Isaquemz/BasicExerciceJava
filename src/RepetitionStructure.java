b89fa8c2e53a95eb7aeca5c8d82b228ed699d4ef
Isaque Menezes <isaquesantos1517@gmail.com>
2024-08-09 16:08:41 -0300

    Estrutura de exercicios para resolução

---
+++ src/RepetitionStructure.java
@@ -0,0 +1,201 @@
+public class RepetitionStructure {
+
+    public static void exerciceOne() {
+        /*
+            Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja
+            inválido e continue pedindo até que o usuário informe um valor válido.
+         */
+
+
+
+    }
+
+    public static void exerciceTwo() {
+        /*
+            Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual
+            ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+         */
+
+
+
+    }
+
+    public static void exerciceThree() {
+        /*
+            Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de
+            crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de crescimento
+            de 1.5%. Faça um programa que calcule e escreva o número de anos necessários para que a
+            população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
+         */
+
+
+
+    }
+
+    public static void exerciceFour() {
+        /*
+            Faça um programa que leia 5 números e informe o maior número.
+         */
+
+
+
+    }
+
+    public static void exerciceFive() {
+        /*
+            Faça um programa que leia 5 números e informe a soma e a média dos números.
+         */
+
+
+
+    }
+
+    public static void exerciceSix() {
+        /*
+            Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+         */
+
+
+
+    }
+
+    public static void exerciceSeven() {
+        /*
+            Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
+            O usuário deve informar de qual número ele deseja ver a tabuada. A saída deve ser conforme
+            o exemplo abaixo:
+            Tabuada de 5:
+                5 X 1 = 5
+                5 X 2 = 10
+                ...
+                5 X 10 = 50
+         */
+
+
+
+    }
+
+    public static void exerciceEight() {
+        /*
+            Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+            Ex.: 5!=5.4.3.2.1=120.
+         */
+
+
+
+    }
+
+    public static void exerciceNine() {
+        /*
+            Faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
+            Um número primo é aquele que é divisível somente por ele mesmo e por 1.
+         */
+
+
+
+    }
+
+    public static void exerciceTen() {
+        /*
+            Faça um programa que calcule a média aritmética de N notas.
+         */
+
+
+
+    }
+
+    public static void exerciceEleven() {
+        /*
+            Faça um programa que peça para n pessoas a sua idade, ao final o programa deverá verificar
+            se a média de idade da turma varia entre 0 e 25, 26 e 60, e maior que 60; e então, dizer
+            se a turma é jovem, adulta ou idosa, conforme a média calculada.
+         */
+
+
+
+    }
+
+    public static void exerciceTwelve() {
+        /*
+            Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores.
+            Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
+         */
+
+
+
+    }
+
+    public static void exerciceThirteen() {
+        /*
+            O Departamento Estadual de Meteorologia lhe contratou para desenvolver um programa que leia
+            um conjunto indeterminado de temperaturas e informe ao final a menor e a maior temperaturas
+            informadas, bem como a média das temperaturas.
+         */
+
+
+
+    }
+
+    public static void exerciceFourteen() {
+        /*
+            Desenvolver um programa para verificar a nota do aluno em uma prova com 10 questões,
+            o programa deve perguntar ao aluno a resposta de cada questão e ao final comparar com o
+            gabarito da prova e assim calcular o total de acertos e a nota (atribuir 1 ponto por resposta certa).
+            Após cada aluno utilizar o sistema, deve ser feita uma pergunta se outro aluno vai utilizar o sistema.
+            Após todos os alunos terem respondido, informar:
+                Maior e Menor Acerto;
+                Total de Alunos que utilizaram o sistema;
+                A Média das Notas da Turma.
+         */
+
+
+
+    }
+
+    public static void exerciceFifteen() {
+        /*
+            Faça um programa que peça um número inteiro positivo e em seguida mostre este número invertido.
+            Exemplo:
+                12376489
+                => 98467321
+         */
+
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("-------- Estruturas Repetição --------\n\n");
+        System.out.println("---------- Exercicio 01 --------------\n");
+        exerciceOne();
+        System.out.println("---------- Exercicio 02 --------------\n");
+        exerciceTwo();
+        System.out.println("---------- Exercicio 03 --------------\n");
+        exerciceThree();
+        System.out.println("---------- Exercicio 04 --------------\n");
+        exerciceFour();
+        System.out.println("---------- Exercicio 05 --------------\n");
+        exerciceFive();
+        System.out.println("---------- Exercicio 06 --------------\n");
+        exerciceSix();
+        System.out.println("---------- Exercicio 07 --------------\n");
+        exerciceSeven();
+        System.out.println("---------- Exercicio 08 --------------\n");
+        exerciceEight();
+        System.out.println("---------- Exercicio 09 --------------\n");
+        exerciceNine();
+        System.out.println("---------- Exercicio 10 --------------\n");
+        exerciceTen();
+        System.out.println("---------- Exercicio 11 --------------\n");
+        exerciceEleven();
+        System.out.println("---------- Exercicio 12 --------------\n");
+        exerciceTwelve();
+        System.out.println("---------- Exercicio 13 --------------\n");
+        exerciceThirteen();
+        System.out.println("---------- Exercicio 14 --------------\n");
+        exerciceFourteen();
+        System.out.println("---------- Exercicio 15 --------------\n");
+        exerciceFifteen();
+    }
+
+}
